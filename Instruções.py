3f6ee7139269037ef7cdb66b9bb0a34b8f7c9221
VITOR SALLES PITHON MACEDO <c2510574@MULHERMARAVILHA.LAB-GRAD>
2025-06-02 12:44:45 -0300

    Novidade

--- Instruções.py
+++ Instruções.py
@@ -1,13 +1,22 @@
 '''
 # O processo a seguiir é todo feito no cmd (Terminal de comandos), para acessar aperte o botao windows e digita cmd
 
-# As 3 primeiras instruções abaixo são usadas para enviar modificações no arquivo ao git principal
+
+# Aqui você conecta o seu pc ao github
+
+Vai apertando 'cd' + nome-da-pasta até a pasta que você for guardar o repositório
+git init  
+git clone https://github.com/pthsl/projetog3.git  # Link do repositório
+cd projetog3
+
+
+# Após iniciar o git na sua pasta , siga as instruções abaixo
+# As 3 primeiras instruções abaixo são usadas para *ENVIAR* modificações no arquivo ao git principal
 
 git add . / git add nome-do-arquivo  # dá git add.  , para adicionar os arquivos do repositório
 git commit -m "mensagem"  # Coloque a mensagem entre aspas, e é obrigatório ter pelo menos uma mensagem
 git push   # Aqui envia o código pro git
 
 
-
-git pull  # Puxa os arquvios modificados no git para o seu repositório
+git pull  # *RECEBE* os arquvios modificados no git para o seu repositório
 '''
