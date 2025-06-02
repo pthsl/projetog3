668859c41b3ed27a4c7d2813fa14741dee6599b1
VITOR SALLES PITHON MACEDO <c2510574@MULHERMARAVILHA.LAB-GRAD>
2025-06-02 12:38:08 -0300

    Novas instruções

--- Instruções.py
+++ Instruções.py
@@ -1,9 +1,13 @@
 '''
 # O processo a seguiir é todo feito no cmd (Terminal de comandos), para acessar aperte o botao windows e digita cmd
 
+# As 3 primeiras instruções abaixo são usadas para enviar modificações no arquivo ao git principal
+
 git add . / git add nome-do-arquivo  # dá git add.  , para adicionar os arquivos do repositório
 git commit -m "mensagem"  # Coloque a mensagem entre aspas, e é obrigatório ter pelo menos uma mensagem
 git push   # Aqui envia o código pro git
 
 
+
+git pull  # Puxa os arquvios modificados no git para o seu repositório
 '''
