ae3e0963cda1fd24062382d6918e297c500cd161
pthsl <vitorpth102@gmail.com>
2025-06-02 12:26:26 -0300

    Update and rename teste.py to Instruções.py

---
+++ Instruções.py
@@ -0,0 +1,6 @@
+'''
+# O processo a seguiir é todo feito no cmd (Terminal de comandos), para acessar aperte o botao windows e digita cmd
+git add . / git add nome-do-arquivo   # dá git add.  , para adicionar os arquivos do repositório
+git commit -m "mensagem"   # Coloque a mensagem entre aspas, e é obrigatório ter pelo menos uma mensagem
+git push   # Aqui envia o código pro git
+'''
--- teste.py
+++
@@ -1,5 +0,0 @@
-'''
-git add . / git add nome-do-arquivo
-git commit -m "mensagem"
-git push 
-'''
