6466a17c053eb76f72c6ece97b1fd5c3be4be2fc
pthsl <vitorpth102@gmail.com>
2025-06-02 12:31:20 -0300

    Update Instruções.py

--- Instruções.py
+++ Instruções.py
@@ -1,7 +1,9 @@
 '''
 # O processo a seguiir é todo feito no cmd (Terminal de comandos), para acessar aperte o botao windows e digita cmd
 
-git add . / git add nome-do-arquivo   # dá git add.  , para adicionar os arquivos do repositório
-git commit -m "mensagem"   # Coloque a mensagem entre aspas, e é obrigatório ter pelo menos uma mensagem
+git add . / git add nome-do-arquivo  # dá git add.  , para adicionar os arquivos do repositório
+git commit -m "mensagem"  # Coloque a mensagem entre aspas, e é obrigatório ter pelo menos uma mensagem
 git push   # Aqui envia o código pro git
+
+git pull  # Para puxar os arquivos modificados para o repositório no pc
 '''
